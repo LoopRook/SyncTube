cfb394e5707ef9637ef3426872fbeb4915495b1a
LoopRook <41098523+LoopRook@users.noreply.github.com>
2025-05-31 16:25:40 -0600

    Update DbManager.hx

--- src/server/DbManager.hx
+++ src/server/DbManager.hx
@@ -1,12 +1,21 @@
 package server;
 
+import js.node.fs;
+import js.node.path;
+
 class DbManager {
     var db:Dynamic;
 
     public function new(path:String) {
+        var dir = path.split('/').slice(0, -1).join('/');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
         var sqlite3 = untyped __js__("require('sqlite3').verbose()");
         db = untyped __js__("new sqlite3.Database(path)");
         trace("DB ready: " + path);
+
         db.run('CREATE TABLE IF NOT EXISTS playlists (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT)');
     }
 
